2558eba1e793fbce95d3d8f4a845d9f1aac3e417
Ved Patel <24bcs188@iiitdmj.ac.in>
2026-03-26 18:13:15 +0530

    test: add unit tests for LocationStateHolder and ServiceEventBus

--- android/app/src/test/java/org/onebusaway/vehiclepositions/util/LocationStateHolderTest.kt
+++ android/app/src/test/java/org/onebusaway/vehiclepositions/util/LocationStateHolderTest.kt
@@ -1,4 +1,19 @@
-package org.onebusaway.vehicle_positions.util
+package org.onebusaway.vehiclepositions.util
+
+import org.junit.Assert.assertNull
+import org.junit.Test
 
 class LocationStateHolderTest {
+
+    private val holder = LocationStateHolder()
+
+    @Test
+    fun `initial location is null`() {
+        assertNull(holder.lastLocation.value)
+    }
+
+    @Test
+    fun `hasLocation returns false when no location set`() {
+        assertNull(holder.lastLocation.value)
+    }
 }
--- android/app/src/test/java/org/onebusaway/vehiclepositions/util/ServiceEventBusTest.kt
+++ android/app/src/test/java/org/onebusaway/vehiclepositions/util/ServiceEventBusTest.kt
@@ -1,4 +1,13 @@
 package org.onebusaway.vehiclepositions.util
 
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
 class ServiceEventBusTest {
+
+    @Test
+    fun `ServiceEventBus initializes without error`() {
+        val bus = ServiceEventBus()
+        assertNotNull(bus)
+    }
 }
